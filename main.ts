a57a2dfb3c0de41098c99c444ca4004ce40250de
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-10-31 14:42:47 -0700

    add dark mode

--- main.ts
+++ main.ts
@@ -1,15 +1,34 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron');
+const path = require('node:path');
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.ts')
+    }
   });
 
   win.loadFile('index.html');
 };
 
 app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong');
+
+  ipcMain.handle('dark-mode:toggle', () => {
+    if (nativeTheme.shouldUseDarkColors) {
+      nativeTheme.themeSource = 'light';
+    } else {
+      nativeTheme.themeSource = 'dark';
+    }
+    return nativeTheme.shouldUseDarkColors;
+  });
+
+  ipcMain.handle('dark-mode:system', () => {
+    nativeTheme.themeSource = 'system';
+  });
+
   createWindow();
 
   app.on('activate', () => {
