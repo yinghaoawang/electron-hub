85b7bb73a74f4f0305ff5b20bbf4cbc17f4e8c39
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-11-01 16:51:04 -0700

    add docker & hot reloading for server

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import fastify from 'fastify';
+
+const server = fastify();
+
+const { ADDRESS = 'localhost', PORT = '8080' } = process.env;
+
+server.get('/', async (request, reply) => {
+  return { message: 'Hello world! yayo' };
+});
+
+server.get('/ping', async (request, reply) => {
+  return 'pong\n';
+});
+
+server.listen({ host: ADDRESS, port: parseInt('3000', 10) }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Fastify server started on ${address}`);
+});
