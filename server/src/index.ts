ee12afd7310131f4aec25fa4164223d6fa04a837
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-11-06 10:09:58 -0800

    add clerk session fetching & docker env config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,25 @@
-import Fastify from 'fastify';
+import 'dotenv/config';
+
+import Fastify, { FastifyRequest } from 'fastify';
 import cors from 'cors';
-import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node';
+import { AuthObject, ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node';
 
 const { ADDRESS = 'localhost', PORT = '8080' } = process.env;
+
+function getAuth(request: FastifyRequest) {
+  const auth = (request.raw as any)?.auth as AuthObject;
+  return auth;
+}
 
 async function build() {
   const fastify = Fastify();
   await fastify.register(require('@fastify/express'));
 
   fastify.use(cors());
+  fastify.use(ClerkExpressRequireAuth());
 
   fastify.get('/', async (request, reply) => {
+    console.log(getAuth(request));
     return { message: 'Hello world!' };
   });
 
