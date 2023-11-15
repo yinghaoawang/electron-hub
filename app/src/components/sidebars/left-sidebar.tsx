0fb6a7ef6af1934a7ea87e6c008771c0e6fc2f1a
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-11-15 14:24:40 -0800

    add placeholder pfp for user tray

--- app/src/components/sidebars/left-sidebar.tsx
+++ app/src/components/sidebars/left-sidebar.tsx
@@ -1,8 +1,10 @@
-
+import { useAuth } from '../../contexts/AuthContext';
 import { useCurrentRoom } from '../../contexts/CurrentRoomContext';
+import placeholderPfp from '../../_assets/placeholder.jpg';
 
 export default function LeftSidebar() {
   const { currentRoom } = useCurrentRoom();
+  const { authUser } = useAuth();
 
   return (
     <div className='left-sidebar shrink-0 w-[250px] h-screen flex flex-col justify-between'>
@@ -16,8 +18,8 @@ export default function LeftSidebar() {
       </div>
       <div className='user-tray flex items-center shrink-0 h-20 p-4 justify-between'>
         <div className='flex gap-3 items-center'>
-          <>User button</>
-          <span className='truncate'>{'user?.fullName'}</span>
+          <img src={placeholderPfp} className='w-10 h-10 rounded-full' />
+          <span className='truncate'>{authUser.displayName}</span>
         </div>
         <div>X</div>
       </div>
