df76e90f4c83bd93f62ccd71242e3c3288c0847e
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-11-14 17:13:32 -0800

    add socket path config vars

--- server/src/socketServer.ts
+++ server/src/socketServer.ts
@@ -2,9 +2,14 @@ import { createServer } from 'http';
 import { Server } from 'socket.io';
 import { createSocketListeners } from './socketListeners';
 
+const { SOCKET_PATH = '/socket' } = process.env;
+
 export function buildSocketServer() {
   const httpServer = createServer();
-  const io = new Server(httpServer, { cors: { origin: '*' } });
+  const io = new Server(httpServer, {
+    path: SOCKET_PATH,
+    cors: { origin: '*' }
+  });
   createSocketListeners(io);
   return httpServer;
 }
