cab220516ab8f156d774dbad4c469dc6d6cbb09b
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-11-16 16:46:39 -0800

    add current room global & highlight in sidebar

--- app/src/components/sidebars/left-sidebar.tsx
+++ app/src/components/sidebars/left-sidebar.tsx
@@ -1,17 +1,24 @@
 import { useAuth } from '../../contexts/AuthContext';
 import { useCurrentRoom } from '../../contexts/CurrentRoomContext';
 import placeholderPfp from '../../_assets/placeholder.jpg';
+import { cn } from '../../_lib/utils';
 
 export default function LeftSidebar() {
-  const { currentRoom } = useCurrentRoom();
+  const { currentRoom, currentChannel } = useCurrentRoom();
   const { authUser } = useAuth();
 
   return (
     <div className='left-sidebar shrink-0 w-[250px] h-screen flex flex-col justify-between'>
       <div className='pt-4 px-4 overflow-auto'>
-        {currentRoom && <div className='text-xl font-semibold'>Channels</div>}
+        {currentRoom && <div className='text-xl font-semibold mb-2'>Channels</div>}
         {currentRoom?.channels.map((channel, index) => (
-          <button key={index} className='block my-1 py-1 truncate'>
+          <button
+            key={index}
+            className={cn(
+              'flex my-1 py-1 truncate w-full px-4 h-10 items-center rounded-md',
+              channel == currentChannel && '!bg-neutral-800'
+            )}
+          >
             <span className='mr-2'>#</span>
             <span>{channel.name}</span>
           </button>
--- app/src/contexts/CurrentRoomContext.tsx
+++ app/src/contexts/CurrentRoomContext.tsx
@@ -1,10 +1,12 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import { Room } from '../../../shared/shared-types';
+import { Channel, Room } from '../../../shared/shared-types';
 import { useRoomData } from './RoomDataContext';
 
 type CurrentRoomContent = {
   currentRoom: Room;
   setCurrentRoomById: (id: bigint) => void;
+  currentChannel: Channel;
+  setCurrentChannelById: (id: bigint) => void;
 };
 
 const CurrentRoomContext = createContext<CurrentRoomContent>(null);
@@ -14,8 +16,12 @@ export function CurrentRoomProvider({
   children: React.ReactNode;
 }) {
   const { roomDataArray, getRoomData } = useRoomData();
+
   const [currentRoom, setCurrentRoom] = useState<Room>(null);
   const [currentRoomId, setCurrentRoomId] = useState<bigint>(null);
+
+  const [currentChannel, setCurrentChannel] = useState<Channel>(null);
+  const [currentChannelId, setCurrentChannelId] = useState<bigint>(null);
 
   useEffect(() => {
     if (currentRoomId == null) {
@@ -24,13 +30,35 @@ export function CurrentRoomProvider({
     }
     const found = getRoomData(BigInt(currentRoomId));
     setCurrentRoom(found);
+    setCurrentChannelById(found?.channels[0]?.id);
   }, [currentRoomId, roomDataArray]);
 
   const setCurrentRoomById = (id: bigint) => {
     setCurrentRoomId(id);
   };
 
-  const value = { currentRoom, setCurrentRoomById };
+  useEffect(() => {
+    if (currentChannelId == null || currentRoom == null) {
+      setCurrentChannel(null);
+      return;
+    }
+    const found = currentRoom.channels?.find(
+      (channel) => channel.id == currentChannelId
+    );
+
+    setCurrentChannel(found);
+  }, [currentRoom, roomDataArray]);
+
+  const setCurrentChannelById = (id: bigint) => {
+    setCurrentChannelId(id);
+  };
+
+  const value = {
+    currentRoom,
+    setCurrentRoomById,
+    currentChannel,
+    setCurrentChannelById
+  };
   return (
     <CurrentRoomContext.Provider value={value}>
       {children}
